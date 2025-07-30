b9348e91bb7aa9286bd93e8572fad01e893e5216
SergioGBosco <sergiogibosco@gmail.com>
2025-07-30 16:29:22 +0200

    Bonus

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello Word")
+let message
+
+if (process.argv[2]) {
+  message = process.argv[2] + `` + process.argv[3]
+}
+
+else {
+  message = `Rimane Hello word `
+}
+console.log(process.argv)
+console.log(message)
